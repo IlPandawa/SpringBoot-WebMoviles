5cb2bbe589db52cccb7f5193fe3a66a7f234d198
Elliot Rendon León <142831325+IlPandawa@users.noreply.github.com>
2025-02-21 10:57:58 -0600

    auth spring

---
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((request) ->
+                //URI desprotegida
+                request.requestMatchers("/").permitAll()
+                        //URI protegida
+                        .anyRequest().authenticated()
+        ).formLogin((form) ->
+                form.loginPage(("/login")).permitAll()
+                        .defaultSuccessUrl("/home", true)
+        ).logout((logout) -> logout.permitAll().logoutSuccessUrl("/"));
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
